abb1898c6aae30636c92506fbd06ebc15249a704
shahrookhansahil <shahroo.bscsf19@iba-suk.edu.pk>
2023-03-07 12:07:10 +0500

    app.js updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [state, setState] = useState(0);
+  useEffect(()=>{
+    async function getData(){
+      const data = await fetch("http://hub.dummyapis.com/employee?noofRecords=100&idstarts=1001");
+    }
+  },[state])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={()=>setState(state+1)}>CLick me</button>
+     <h1>{state}</h1>
     </div>
   );
 }
